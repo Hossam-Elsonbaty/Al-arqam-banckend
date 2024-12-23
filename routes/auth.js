6c25d3fd6c63f045c47a007ed0f5b0b999e27a78
Hossam-Elsonbaty <107892692+Hossam-Elsonbaty@users.noreply.github.com>
2024-12-23 07:35:43 +0200

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -178,8 +178,7 @@ router.post('/contact-us', async (req, res) => {
   }
 })
 
-
-router.post('/send-email', verifyToken, async (req, res) => {
+router.post('/send-email', verifyToken,async (req, res) => {
   const data = req.body;
   console.log(data);
   if (!data.emailAddress || !data.emailMessage || !data.emailSubject) {
@@ -193,8 +192,7 @@ router.post('/send-email', verifyToken, async (req, res) => {
       text: `${data.emailMessage}`,
       html: `
               <h1>Hello from Alarqam Academy</h1>
-              <p>We will be so happy if you accepted our invitation to this party</p>
-            `
+              <p>We will be so happy if you accepted our invitation to this party</p>            `
     };
     await sgMail.send(msg)
     .then((res)=>{console.log(res);})
@@ -206,6 +204,34 @@ router.post('/send-email', verifyToken, async (req, res) => {
     res.status(500).json({ message: 'Server error, please try again later.' });
   }
 })
+
+// router.post('/send-email', verifyToken, async (req, res) => {
+//   const data = req.body;
+//   console.log(data);
+//   if (!data.emailAddress || !data.emailMessage || !data.emailSubject) {
+//     return res.status(400).json({ message: 'Please fill in all fields.' });
+//   }
+//   try {
+//     const msg = {
+//       to: `${data.emailAddress}`, // Receiver's email
+//       from: 'armaggg3@gmail.com', // Use a verified sender
+//       subject: `${data.emailSubject}`,
+//       text: `${data.emailMessage}`,
+//       html: `
+//               <h1>Hello from Alarqam Academy</h1>
+//               <p>We will be so happy if you accepted our invitation to this party</p>
+//             `
+//     };
+//     await sgMail.send(msg)
+//     .then((res)=>{console.log(res);})
+//     .catch((err)=>{console.log(err.message);})
+//     // Send the email
+//     res.status(201);
+//   } catch (error) {
+//     console.error('Error saving contact or sending email:', error);
+//     res.status(500).json({ message: 'Server error, please try again later.' });
+//   }
+// })
 
 
 router.post('/student-application', async (req, res) => {
