b91a84453433d81396881301b7237ce8323e9277
Hossam-Elsonbaty <107892692+Hossam-Elsonbaty@users.noreply.github.com>
2024-12-23 04:23:52 +0200

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -23,15 +23,12 @@ router.post('/login', async (req, res) => {
       console.log("Invalid username or password");
       return res.status(400).json({ message: 'Invalid username or password' });
     }
-    
-    // Compare plain text password with stored hashed password
     const isMatch = await bcrypt.compare(password, user.password);
     console.log(isMatch, password, user.password);
     if (!isMatch) {
       console.log('Invalid username or password', typeof password, typeof user.password);
       return res.status(400).json({ message: 'Invalid username or password' });
     }
-    
     // Generate JWT token
     const token = jwt.sign({ id: user._id }, JWT_SECRET, { expiresIn: TOKEN_EXPIRY });
     res.json({ token, message: 'Login successful' });
@@ -40,31 +37,6 @@ router.post('/login', async (req, res) => {
     res.status(500).json({ message: 'Server error' });
   }
 });
-
-// router.post('/login', async (req, res) => {
-//   const { username, password } = req.body;
-//   console.log(username, password);
-//   try {
-//     const user = await usersModel.findOne({ username });
-//     console.log("user:",user);
-//     if (!user) {
-//       console.log("Invalid username or password");
-//       return res.status(400).json({ message: 'Invalid username or password' });
-//     }
-//     const hashedPassword = await bcrypt.hash(password, 10);
-//     const isMatch = bcrypt.compare(hashedPassword, user.password);
-//     console.log(isMatch,hashedPassword, user.password);
-//     if (!isMatch) {
-//       console.log('Invalid match username or password',typeof password, typeof user.password);
-//       return res.status(400).json({ message: 'Invalid username or password' });
-//     }
-//     const token = jwt.sign({ id: user._id }, JWT_SECRET, { expiresIn: TOKEN_EXPIRY });
-//     res.json({ token, message: 'Login successful' });
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ message: 'Server error' });
-//   }
-// });
 
 router.get('/users', verifyToken, async (req, res) => {
   console.log('Incoming request to /users');
@@ -205,7 +177,9 @@ router.post('/contact-us', async (req, res) => {
     res.status(500).json({ message: 'Server error, please try again later.' });
   }
 })
-router.post('/send-email', async (req, res) => {
+
+
+router.post('/send-email', verifyToken, async (req, res) => {
   const data = req.body;
   console.log(data);
   if (!data.emailAddress || !data.emailMessage || !data.emailSubject) {
@@ -231,6 +205,8 @@ router.post('/send-email', async (req, res) => {
     res.status(500).json({ message: 'Server error, please try again later.' });
   }
 })
+
+
 router.post('/student-application', async (req, res) => {
   const application = req.body;
   console.log(application);
