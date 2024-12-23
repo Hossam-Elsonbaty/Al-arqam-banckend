cec9e90443e32e5fe3becf822af07e8f2d7c6062
Hossam-Elsonbaty <107892692+Hossam-Elsonbaty@users.noreply.github.com>
2024-12-23 02:53:26 +0200

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -25,7 +25,7 @@ router.post('/login', async (req, res) => {
     }
     const hashedPassword = await bcrypt.hash(password, 10);
     const isMatch = bcrypt.compare(hashedPassword, user.password);
-    console.log(isMatch,password, user.password);
+    console.log(isMatch,hashedPassword, user.password);
     if (!isMatch) {
       console.log('Invalid match username or password',typeof password, typeof user.password);
       return res.status(400).json({ message: 'Invalid username or password' });
