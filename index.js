0add13116d1dd51206c99bf4cd3bf8dbdbd41245
Hossam-Elsonbaty <107892692+Hossam-Elsonbaty@users.noreply.github.com>
2024-12-23 07:27:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -21,15 +21,6 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: '*',
 }));
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', req.headers.origin); // Allow dynamic origin
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(204); // Respond to preflight
-  }
-  next();
-});
 const PORT = process.env.PORT || 5555;
 
 // Nodemailer transporter
