7dcb58ce8b1e286ec263b87c4f1b22f475fc5fc2
Hossam-Elsonbaty <107892692+Hossam-Elsonbaty@users.noreply.github.com>
2025-01-29 21:34:14 +0200

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ const corsOptions = {
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: '*',
 };
-app.set('trust proxy', true);
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 100, // Limit each IP to 100 requests per windowMs
